0411f97ca076c4d551acd7b206d5fa2dd1710340
wataruw <saruchichi@gmail.com>
2025-05-02 13:31:49 +0900

    KC_BTN1,KC_BTN2を入れ替え

--- keymaps/naginata_v16/keymap.c
+++ keymaps/naginata_v16/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LNG2, KC_LNG1, JP_CIRC, JP_YEN,  KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_UP, KC_MS_WH_RIGHT,  KC_PGUP,
         KC_HOME, JP_LBRC, JP_RBRC, JP_AT,   KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_LEFT,       KC_DOWN,       KC_UP,       KC_RGHT,         KC_END,
         KC_PSCR, JP_COLN, JP_SCLN, JP_BSLS, KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_MS_LEFT,    KC_MS_DOWN,    KC_MS_UP,    KC_MS_RIGHT,     KC_PGDN,
-        KC_LSFT, KC_LGUI, KC_LALT, KC_TAB,    TO(0),     KC_BTN1,   KC_BTN2,  KC_BSPC,         KC_DEL,        TO(2),       KC_LCTL,         TO(1)
+        KC_LSFT, KC_LGUI, KC_LALT, KC_TAB,    TO(0),     KC_BTN2,   KC_BTN1,  KC_BSPC,         KC_DEL,        TO(2),       KC_LCTL,         TO(1)
     ),
     [2] = LAYOUT( /* etc. */
         QK_BOOT, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
